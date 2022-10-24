ae7c97637688400da2c5dcfa6bf3cbb77eb386af
Pawat4988 <59772273+Pawat4988@users.noreply.github.com>
2022-10-25 01:28:50 +0700

    rough IF handle

--- Assets/Scripts/MachineActivationManager.cs
+++ Assets/Scripts/MachineActivationManager.cs
@@ -6,8 +6,9 @@ public class MachineActivationManager : MonoBehaviour
 {
 
     public static MachineActivationManager Instance;
-    public int tick = 0;
     public static List<GameObject> allMachineList = new List<GameObject>();
+    public static List<OrderManager> allOrderManagerList = new List<OrderManager>();
+    public static List<GameObject> activatedMachineList = new List<GameObject>();
     public int startOrder = 1;
 
 
@@ -33,35 +34,44 @@ public class MachineActivationManager : MonoBehaviour
         }
     }
 
-    void nextTick()
-    {
-        tick++;
-    }
-
+    // add when IF get activated
     void addOrderManager()
     {
         // create orderManger and add to a list
     }
 
+    // use a button or something to activate this function to start activating machines
     void doCurrentOrder()
     {
         // loop through orderManager list and activate all of them *make sure to not activate same machine*
         // keep list of machine activated
         // make sure all is done
+        foreach (OrderManager orderManager in allOrderManagerList)
+        {
+            orderManager.activateCurrentOrder();
+        }
+        waitAllMachineDone();
+        endOfOrderReset();
     }
 
-    void waitAllMachineDone()
+    public void waitAllMachineDone()
     {
 
+    }
+
+    void endOfOrderReset()
+    {
+        // reset all the temp list used
+        activatedMachineList.Clear();
     }
 
     
 
-    public class orderManager
+    public class OrderManager
     {
         public int currentOrder = 1;
 
-        public orderManager(int currentOrder)
+        public OrderManager(int currentOrder)
         {
             this.currentOrder = currentOrder;
         }
@@ -75,15 +85,63 @@ public class MachineActivationManager : MonoBehaviour
             else { return false; }
         }
 
+        bool isNextOrder(GameObject machine)
+        {
+            if (machine.GetComponent<Machine>().order == currentOrder+1)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
         List<GameObject> getMachinesWithCurrentOrder()
         {
             List<GameObject> result = new List<GameObject>(allMachineList.FindAll(isCurrentOrder));
             return result;
         }
 
-        void activateCurrentOrder()
+        List<GameObject> getMachinesWithNextOrder()
         {
+            List<GameObject> result = new List<GameObject>(allMachineList.FindAll(isNextOrder));
+            return result;
+        }
+
+        public void activateCurrentOrder()
+        {
+            List<GameObject>  machinesToActivate = getMachinesWithCurrentOrder();
             // send signal to machine in current order
+            foreach (GameObject machine in machinesToActivate)
+            {
+                if (!activatedMachineList.Contains(machine))
+                {
+                    if (machine.GetComponent<Machine>().type == MachineType.If)
+                    {
+                        activateIFMachine(machine);
+                    }
+                    else
+                    {
+                        activateMachine(machine);
+                    }
+                    activatedMachineList.Add(machine);
+                }
+            }
+            currentOrder++;
+        }
+
+        public void activateMachine(GameObject machine)
+        {
+            // activate the machine
+        }
+
+        public void activateIFMachine(GameObject machine)
+        {
+            List<GameObject> nextMachinesToActivate = getMachinesWithNextOrder();
+            // if machine activated by IF is not in next order list
+            if (!nextMachinesToActivate.Contains(machine))
+            {
+                Instance.addOrderManager();
+            }
+            // activate the IF machine
         }
     }
 }
